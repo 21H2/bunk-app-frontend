0b07633fa1a75c8e28cc6af3a945a0079d4c8931
21H2 <83266784+21H2@users.noreply.github.com>
2026-01-08 11:04:00 +0000

    Bunk frontend clean

---
+++ components/navigation/right-panel.tsx
@@ -0,0 +1,161 @@
+import { useRef } from 'react';
+import { View } from 'react-native';
+import { DefaultText } from '../default-text';
+import { getRandomElement } from '../../util/util';
+import { PARTNER_URL } from '../../env/env';
+
+type Partner = {
+  name: string
+  file: string
+  link: string
+};
+
+const partners: Partner[] = [
+  { name: 'SFDating',  file: 'sfdating.jpg',  link: 'https://discord.gg/REbbHqzD9p'},
+  { name: 'duo3k',     file: 'duo3k.webp',    link: 'https://discord.gg/duo3k'},
+  { name: 'Tiblur',    file: 'tiblur.jpg',    link: 'https://tiblur.com/register' },
+  { name: 'gg/redux',  file: 'redux.png',     link: 'https://discord.gg/EA3QYF9apJ' },
+  { name: 'NEET_Chat', file: 'neet-chat.png', link: 'https://discord.gg/96JShH3N7Y' },
+  { name: 'Affinity',  file: 'affinity.png',  link: 'https://discord.gg/pvQ9EMVVq5' },
+];
+
+const DuoliciousRightPanelContent = () => {
+  return (
+    <View
+      style={{
+        borderRadius: 10,
+        backgroundColor: '#70f',
+        width: '100%',
+        padding: 20,
+        gap: 20,
+        alignItems: 'center',
+      }}
+    >
+      <DefaultText
+        style={{
+          color: 'white',
+          fontWeight: '900',
+          fontSize: 22,
+          textAlign: 'center',
+        }}
+      >
+        Your Social Group Here
+      </DefaultText>
+
+      <DefaultText
+        style={{
+          color: 'white',
+          textAlign: 'center',
+          backgroundColor: 'black',
+          borderRadius: 10,
+          padding: 14,
+        }}
+      >
+        Do you have a Discord server, Reddit sub, forum or other social group
+        you want to promote? You can do it here, for free!
+        {'\n\n'}
+        What’s the catch? You’ll have to promote Duolicious back. (Plus your
+        group should be something Duolicious members would like.)
+        {'\n\n'}
+        Inquiries: {}
+        <DefaultText
+          style={{
+            color: 'white',
+            fontWeight: '700'
+          }}
+          // @ts-ignore
+          href="mailto:admin@duolicious.app"
+        >
+          admin@duolicious.app
+        </DefaultText>
+      </DefaultText>
+    </View>
+  );
+};
+
+const SponsoredRightPanelContent = () => {
+  const partner = useRef(getRandomElement(partners)).current;
+
+  if (!partner) {
+    return null;
+  }
+
+  return (
+    <View
+      style={{
+        width: 300,
+        gap: 10,
+        justifyContent: 'center',
+      }}
+    >
+      <a
+        href={partner.link}
+        target="_blank"
+        style={{
+          display: 'block',
+          width: '100%',
+          height: 250,
+          borderWidth: 1,
+          borderColor: 'black',
+          borderStyle: 'solid',
+        }}
+      >
+        <img
+          src={`${PARTNER_URL}/${partner.file}`}
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+        />
+      </a>
+
+      <DefaultText style={{ color: '#999', textAlign: 'center' }}>
+        <DefaultText style={{ fontWeight: '700' }}>
+        {partner.name}
+        </DefaultText>
+        {} is a Duolicious partner
+      </DefaultText>
+
+      <DefaultText style={{ textAlign: 'center' }}>
+        Want to promote your social group for free? Inquire at {}
+        <DefaultText
+          style={{
+            fontWeight: '700'
+          }}
+          // @ts-ignore
+          href="mailto:admin@duolicious.app"
+        >
+          admin@duolicious.app
+        </DefaultText>
+        .
+      </DefaultText>
+    </View>
+  );
+};
+
+const RightPanelContent = () => {
+  const rand = useRef(Math.random()).current;
+
+  if (rand < 0.2) {
+    return <DuoliciousRightPanelContent/>;
+  } else {
+    return <SponsoredRightPanelContent/>;
+  }
+};
+
+const RightPanel = () => {
+  return (
+    <View
+      style={{
+        maxWidth: 360,
+        padding: 20,
+      }}
+    >
+      <RightPanelContent/>
+    </View>
+  );
+};
+
+export {
+  RightPanel,
+};
---
+++ components/welcome-screen.tsx
@@ -0,0 +1,707 @@
+import {
+  Animated,
+  Keyboard,
+  Linking,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  View,
+  useWindowDimensions,
+} from 'react-native';
+import {
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { DefaultText } from './default-text';
+import { DefaultTextInput } from './default-text-input';
+import { ButtonWithCenteredText } from './button/centered-text';
+import { createAccountOptionGroups } from '../data/option-groups';
+import { OptionScreen } from './option-screen';
+import { japi } from '../api/api';
+import { sessionToken } from '../kv-storage/session-token';
+import { Logo16 } from './logo';
+import { KeyboardDismissingView } from './keyboard-dismissing-view';
+import { otpDestination } from '../App';
+import { useSignedInUser } from '../events/signed-in-user';
+import { joinClub } from '../club/club';
+import { isMobile } from '../util/util';
+
+const activeMembersText = (
+  numActiveMembers: number,
+  minActiveMembers: number,
+  minText: string,
+) => {
+  if (numActiveMembers < minActiveMembers) {
+    return minText;
+  } else {
+    return (
+      `${numActiveMembers.toLocaleString()} active member` +
+      (numActiveMembers === 1 ? '' : 's')
+    );
+  }
+};
+
+const ActiveMembers = ({
+  numActiveMembers,
+  minActiveMembers,
+  color,
+  minText = 'on the Duolicious dating app',
+}: {
+  numActiveMembers: number,
+  minActiveMembers: number
+  color: string,
+  minText?: string,
+}) => {
+  const opacity = useRef(new Animated.Value(1)).current;
+
+  const [displayText, setDisplayText] = useState(
+    activeMembersText(numActiveMembers, minActiveMembers, minText));
+
+  const [nextDisplayText, setNextDisplayText] = useState(displayText);
+
+  const isFirstRender = useRef(true);
+
+  useEffect(() => {
+    setNextDisplayText(
+      activeMembersText(numActiveMembers, minActiveMembers, minText));
+  }, [numActiveMembers, minActiveMembers]);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false; // Mark as not first render anymore
+      return; // Skip animation on first render
+    }
+
+    // Fade out, change text, and fade in sequence
+    Animated.sequence([
+      // Fade out
+      Animated.timing(opacity, {
+        toValue: 0,
+        duration: 250,
+        useNativeDriver: true
+      })
+    ]).start(() => {
+      setDisplayText(nextDisplayText);
+
+      // Fade in
+      Animated.timing(opacity, {
+        toValue: 1,
+        duration: 250,
+        useNativeDriver: true
+      }).start();
+    });
+  }, [nextDisplayText]);
+
+  return (
+    <Animated.Text
+      style={{
+        textAlign: 'center',
+        color,
+        opacity,
+        fontFamily: 'MontserratRegular',
+      }}
+    >
+      {displayText}
+    </Animated.Text>
+  );
+};
+
+const Stack = createNativeStackNavigator();
+
+const WelcomeScreen = () => {
+  const { width: windowWidth } = useWindowDimensions();
+
+  return (
+    <View
+      style={{
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#70f',
+        overflow: 'hidden',
+      }}
+    >
+      {Platform.OS === 'web' && windowWidth >= 950 && <>
+        <img
+          src="https://duolicious.app/assets/landing/left.svg"
+          style={{
+            position: 'absolute',
+            bottom: '0',
+            left: `-${75 + Math.max(0, (1380 - windowWidth) / 2)}px`,
+            width: '500px',
+            height: 'auto',
+            objectFit: 'cover',
+            backfaceVisibility: 'hidden',
+            transition: 'transform 0.5s ease-out',
+          }}
+        >
+        </img>
+        <img
+          src="https://duolicious.app/assets/landing/right.svg"
+          style={{
+            position: 'absolute',
+            bottom: '0',
+            right: `-${75 + Math.max(0, (1380 - windowWidth) / 2)}px`,
+            width: '500px',
+            height: 'auto',
+            objectFit: 'cover',
+            backfaceVisibility: 'hidden',
+            transition: 'transform 0.5s ease-out',
+          }}
+        >
+        </img>
+      </>}
+      <View
+        style={{
+          height: '100%',
+          width: '100%',
+          maxWidth: 600,
+          alignSelf: 'center',
+          overflow: 'visible',
+        }}
+      >
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+            animation: 'slide_from_right',
+          }}
+        >
+          <Stack.Screen name="Welcome Screen" component={WelcomeScreen_} />
+          <Stack.Screen name="Create Account Or Sign In Screen" component={OptionScreen} />
+        </Stack.Navigator>
+      </View>
+    </View>
+  );
+};
+
+const InviteScreen = ({navigation, route}) => {
+  const [loading, setLoading] = useState(false);
+  const [signedInUser] = useSignedInUser();
+
+  const clubName = route.params?.clubName as string | undefined;
+  const [numUsers, setNumUsers] = useState<number | undefined>(undefined);
+
+  if (typeof clubName !== 'string') {
+    throw new Error('clubName should be a string');
+  }
+
+  const submit = async () => {
+    if (signedInUser) {
+      setLoading(true);
+
+      await joinClub(clubName, numUsers ?? 0, true);
+
+      setLoading(false);
+
+      if (Platform.OS === 'web') {
+        history.pushState((history?.state ?? 0) + 1, "", "/#");
+      }
+
+      navigation.reset({
+        routes: [
+          {
+            name: "Home",
+            state: {
+              routes: [
+                {
+                  name: "Search"
+                }
+              ]
+            }
+          }
+        ]
+      });
+    } else {
+      navigation.reset({
+        routes: [
+          {
+            name: 'Welcome',
+            state: {
+              routes: [
+                {
+                  name: 'Welcome Screen',
+                  params: { clubName, numUsers }
+                },
+              ]
+            },
+          },
+        ],
+      });
+    }
+  };
+
+  useEffect(() => {
+    const updateNumUsers = async () => {
+      const response = await japi(
+          'GET',
+          '/stats?club-name=' + encodeURIComponent(clubName));
+
+      if (!response.ok)
+        return;
+
+      setNumUsers(response.json.num_active_users);
+    };
+
+    if (numUsers === undefined) {
+      updateNumUsers();
+    }
+  }, [clubName, numUsers]);
+
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: '#70f',
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <KeyboardDismissingView
+        style={{
+          width: '100%',
+          height: '100%',
+          maxWidth: 600,
+          alignSelf: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <View
+          style={{
+            marginTop: 10 + (Platform.OS === 'web' ? 0 : StatusBar.currentHeight ?? 0),
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: 1,
+          }}
+        >
+          <Logo16 size={32} rectSize={0.3} />
+          <Text
+            style={{
+              color: 'white',
+              alignSelf: 'center',
+              fontFamily: 'TruenoBold',
+              fontSize: 20,
+            }}
+            selectable={false}
+          >
+            Duolicious
+          </Text>
+        </View>
+        <View
+          style={{
+            alignSelf: 'center',
+            justifyContent: 'center',
+            flex: 1,
+            width: '100%',
+            paddingHorizontal: 10,
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: 'white',
+              borderRadius: 10,
+              shadowOffset: {
+                width: 0,
+                height: 2,
+              },
+              shadowOpacity: 0.5,
+              shadowRadius: 10,
+              elevation: 3,
+              paddingTop: 20,
+              width: '100%',
+            }}
+          >
+            <View
+              style={{
+                alignSelf: 'center',
+                alignItems: 'center',
+                justifyContent: 'center',
+                gap: 10,
+                width: '100%',
+                paddingHorizontal: 10,
+              }}
+            >
+              <DefaultText
+                style={{
+                  textAlign: 'center',
+                  color: '#555',
+                }}
+              >
+                You’re invited to join
+              </DefaultText>
+              <DefaultText
+                style={{
+                  textAlign: 'center',
+                  color: 'black',
+                  fontSize: 26,
+                  fontFamily: 'MontserratBlack',
+                  flexShrink: 1,
+                  width: '100%',
+                }}
+              >
+                {clubName}
+              </DefaultText>
+              <ActiveMembers
+                numActiveMembers={numUsers ?? -1}
+                minActiveMembers={10}
+                color="#555"
+              />
+            </View>
+            <View
+              style={{
+                justifyContent: 'center',
+                padding: 20,
+                paddingTop: 40,
+                alignSelf: 'flex-start',
+                width: '100%',
+              }}
+            >
+              <ButtonWithCenteredText
+                onPress={submit}
+                borderWidth={0}
+                loading={loading}
+              >
+                <DefaultText disableTheme style={{fontWeight: '700'}}>
+                  Accept Invite
+                </DefaultText>
+              </ButtonWithCenteredText>
+              <DefaultText
+                style={{
+                  color: '#777',
+                  textAlign: 'center',
+                  alignSelf: 'center',
+                  lineHeight: 28,
+                }}
+              >
+                By joining you agree to our {}
+                <DefaultText
+                  disableTheme
+                  style={{
+                    fontWeight: '600',
+                  }}
+                  onPress={() => Linking.openURL('https://duolicious.app/terms')}
+                >
+                  Terms
+                </DefaultText>
+                {}, {}
+                <DefaultText
+                  disableTheme
+                  style={{ fontWeight: '600' }}
+                  onPress={() => Linking.openURL('https://duolicious.app/privacy')}
+                >
+                  Privacy Policy
+                </DefaultText>
+                {} and {}
+                <DefaultText
+                  disableTheme
+                  style={{ fontWeight: '600' }}
+                  onPress={() => Linking.openURL('https://duolicious.app/guidelines')}
+                >
+                  Community Guidelines
+                </DefaultText>
+              </DefaultText>
+            </View>
+          </View>
+        </View>
+      </KeyboardDismissingView>
+    </SafeAreaView>
+  );
+};
+
+const WelcomeScreen_ = ({navigation, route}) => {
+  const clubName_ = (route.params?.clubName) as string | undefined;
+  const [numUsers, setNumUsers] = useState<number | undefined>(route.params?.numUsers);
+
+  const [email, setEmail] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [loginStatus, setLoginStatus] = useState("")
+
+  const { height: windowHeight } = useWindowDimensions();
+
+  const setEmailSafely = (e: string) => {
+    const sanitizedText = e.replace(/\s/g, '');
+    setEmail(sanitizedText);
+  };
+
+  const submit = async (suffix?: string) => {
+    const suffix_ = suffix ?? '';
+    const email_ = email + (email.endsWith(suffix_) ? '': suffix_);
+
+    setLoginStatus("");
+    setIsLoading(true);
+    setEmailSafely(email_);
+    otpDestination.value = email_;
+
+    Keyboard.dismiss();
+
+    const response = await japi(
+      'post',
+      '/request-otp',
+      {
+        email: email_,
+        ...(clubName_ && { pending_club_name: clubName_ }),
+      },
+      { timeout: 9999 * 1000 },
+    );
+
+    setIsLoading(false);
+
+    if (response.ok) {
+      await sessionToken(response.json.session_token);
+
+      if (Platform.OS === 'web') {
+        history.pushState((history?.state ?? 0) + 1, "", "/#");
+      }
+
+      navigation.navigate(
+        'Create Account Or Sign In Screen',
+        {
+          optionGroups: createAccountOptionGroups,
+          showSkipButton: false,
+          showCloseButton: false,
+          showBackButton: true,
+          buttonBorderWidth: 0,
+          backgroundColor: '#7700ff',
+          color: '#ffffff',
+        },
+      );
+    } else {
+      setLoginStatus(
+        response.status === 400 ? 'We don’t support that email provider' :
+        response.status === 429 ? 'You’re doing that too much' :
+        response.status === 460 ? 'Network blocked. Are you using a VPN?' :
+        response.status === 461 ? 'Your account is banned' :
+        'We couldn’t connect to Duolicious'
+      );
+    }
+  };
+
+  useEffect(() => {
+    const updateNumUsers = async () => {
+      const response = await japi('GET', '/stats');
+
+      if (!response.ok)
+        return;
+
+      setNumUsers(response.json.num_active_users);
+    };
+
+    if (numUsers === undefined) {
+      updateNumUsers();
+    }
+  }, [numUsers]);
+
+  const SuffixButton = useCallback(({suffix}) => (
+    <ButtonWithCenteredText
+      onPress={() => !isLoading && submit(suffix)}
+      borderWidth={0}
+      secondary={true}
+      containerStyle={{
+        marginTop: 0,
+        marginBottom: 0,
+        height: undefined,
+      }}
+      backgroundColor="rgb(228, 204, 255)"
+      textStyle={{
+        padding: 10,
+        fontSize: 12,
+        color: '#70f',
+      }}
+    >
+      {suffix}
+    </ButtonWithCenteredText>
+  ), [isLoading, submit]);
+
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: '#70f',
+        width: '100%',
+        height: '100%',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        overflow: 'hidden',
+      }}
+    >
+      <KeyboardDismissingView
+        style={{
+          width: '100%',
+          height: '100%',
+          alignSelf: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <View
+          style={{
+            marginTop: 10 + (Platform.OS === 'web' ? 0 : StatusBar.currentHeight ?? 0),
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: 1,
+          }}
+        >
+          <Logo16 size={32} rectSize={0.3} doAnimate={true} />
+          <Text
+            style={{
+              color: 'white',
+              alignSelf: 'center',
+              fontFamily: 'TruenoBold',
+              fontSize: 20,
+            }}
+            selectable={false}
+          >
+            Duolicious
+          </Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            alignSelf: 'center',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: 10,
+          }}
+        >
+          <DefaultText
+            style={{
+              width: 320,
+              textAlign: 'center',
+              color: 'white',
+              fontSize: 26,
+              fontFamily: 'MontserratBlack',
+            }}
+          >
+            {clubName_ ?
+              `Join ${clubName_} on Duolicious` :
+              'Cute dates & dank memes await...'}
+          </DefaultText>
+          {windowHeight > 500 &&
+            <ActiveMembers
+              numActiveMembers={numUsers ?? -1}
+              minActiveMembers={0}
+              color="white"
+              minText={'\xa0'}
+            />
+          }
+        </View>
+        <View style={{
+          justifyContent: 'flex-start',
+          flex: 1,
+        }}>
+          <DefaultTextInput
+            placeholder="Enter your email to begin"
+            keyboardType="email-address"
+            textContentType="emailAddress"
+            autoComplete="email"
+            autoCapitalize="none"
+            inputMode="email"
+            value={email}
+            onChangeText={setEmailSafely}
+            onSubmitEditing={isMobile() ? undefined : () => submit()}
+            autoFocus={Platform.OS !== 'ios'}
+          />
+          <DefaultText
+            style={{
+              alignSelf: 'center',
+              marginTop: 5,
+              marginLeft: 20,
+              marginRight: 20,
+              color: 'white',
+              opacity: loginStatus !== "" ? 1 : 0
+            }}
+          >
+            {loginStatus || '\xa0'}
+          </DefaultText>
+          {windowHeight > 500 &&
+            <View
+              style={{
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+                marginTop: 5,
+                marginLeft: 20,
+                marginRight: 20,
+                gap: 10,
+              }}
+            >
+              <SuffixButton suffix="@gmail.com" />
+              <SuffixButton suffix="@proton.me" />
+              <SuffixButton suffix="@yahoo.com" />
+              <SuffixButton suffix="@hotmail.com" />
+              <SuffixButton suffix="@outlook.com" />
+            </View>
+          }
+        </View>
+        <View
+          style={{
+            justifyContent: 'center',
+            paddingHorizontal: 20,
+            paddingBottom: 10,
+            alignSelf: 'flex-start',
+            width: '100%',
+          }}
+        >
+          <ButtonWithCenteredText
+            onPress={() => submit()}
+            secondary={true}
+            loading={isLoading}
+            extraChildren={
+              <View style={{ flexDirection: 'row' }}>
+                <DefaultText style={{ fontSize: 16, textAlign: 'center', fontWeight: '700' }}>
+                  Sign Up
+                </DefaultText>
+                <DefaultText style={{ fontSize: 16 }}>
+                  {} or {}
+                </DefaultText>
+                <DefaultText style={{ fontSize: 16, textAlign: 'center', fontWeight: '700' }}>
+                  Sign In
+                </DefaultText>
+              </View>
+            }
+          />
+          <DefaultText
+            style={{
+              color: 'white',
+              textAlign: 'center',
+              alignSelf: 'center',
+              lineHeight: 28,
+            }}
+          >
+            By signing up you agree to our {}
+            <DefaultText
+              disableTheme
+              style={{
+                fontWeight: '600',
+              }}
+              onPress={() => Linking.openURL('https://duolicious.app/terms')}
+            >
+              Terms
+            </DefaultText>
+            {}, {}
+            <DefaultText
+              disableTheme
+              style={{ fontWeight: '600' }}
+              onPress={() => Linking.openURL('https://duolicious.app/privacy')}
+            >
+              Privacy Policy
+            </DefaultText>
+            {} and {}
+            <DefaultText
+              disableTheme
+              style={{ fontWeight: '600' }}
+              onPress={() => Linking.openURL('https://duolicious.app/guidelines')}
+            >
+              Community Guidelines
+            </DefaultText>
+          </DefaultText>
+        </View>
+      </KeyboardDismissingView>
+    </SafeAreaView>
+  );
+};
+
+export {
+  InviteScreen,
+  WelcomeScreen,
+};
