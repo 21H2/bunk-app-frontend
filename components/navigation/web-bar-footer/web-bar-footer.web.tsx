2d164f8253ac1fcddfbcaf3d4930676eac61cd04
21H2 <83266784+21H2@users.noreply.github.com>
2026-01-29 10:38:18 +0530

    links

--- components/navigation/web-bar-footer/web-bar-footer.web.tsx
+++ components/navigation/web-bar-footer/web-bar-footer.web.tsx
@@ -1,38 +1,38 @@
-import { AppStoreBadges } from '../../badges/app-store/app-store';
-const twitterIcon = require('../../../assets/social/twitter-white.svg');
-const redditIcon = require('../../../assets/social/reddit-white.svg');
-const githubIcon = require('../../../assets/social/github-white.svg');
+import { AppStoreBadges } from "../../badges/app-store/app-store";
+const twitterIcon = require("../../../assets/social/twitter-white.svg");
+const redditIcon = require("../../../assets/social/reddit-white.svg");
+const githubIcon = require("../../../assets/social/github-white.svg");
 
 const SocialBadges = () => {
   return (
     <ul
       style={{
-        boxSizing: 'border-box',
-        flexWrap: 'wrap',
-        listStyleType: 'none',
-        width: '100%',
-        justifyContent: 'center',
-        display: 'flex',
-        gap: '24px',
-        border: 'none',
-        padding: '0',
+        boxSizing: "border-box",
+        flexWrap: "wrap",
+        listStyleType: "none",
+        width: "100%",
+        justifyContent: "center",
+        display: "flex",
+        gap: "24px",
+        border: "none",
+        padding: "0",
         marginTop: 8,
         marginBottom: 8,
       }}
     >
       <li>
-        <a target="_blank" href="https://twitter.com/duoliciousapp">
-          <img src={twitterIcon.uri} style={{ height: '20px' }} />
+        <a target="_blank" href="https://twitter.com/bunkappin">
+          <img src={twitterIcon.uri} style={{ height: "20px" }} />
         </a>
       </li>
       <li>
-        <a target="_blank" href="https://www.reddit.com/r/duolicious">
-          <img src={redditIcon.uri} style={{ height: '20px' }} />
+        <a target="_blank" href="https://www.instagram.com/bunkapp.in">
+          <img src={redditIcon.uri} style={{ height: "20px" }} />
         </a>
       </li>
       <li>
-        <a target="_blank" href="https://github.com/duolicious">
-          <img src={githubIcon.uri} style={{ height: '20px' }} />
+        <a target="_blank" href="https://github.com/Bunk-Labs">
+          <img src={githubIcon.uri} style={{ height: "20px" }} />
         </a>
       </li>
     </ul>
@@ -43,21 +43,19 @@ const WebBarFooter = () => {
   return (
     <div
       style={{
-        width: '100%',
-        marginLeft: '15px',
-        marginRight: '15px',
-        justifyContent: 'center',
-        alignItems: 'center',
-        display: 'flex',
-        flexDirection: 'column',
+        width: "100%",
+        marginLeft: "15px",
+        marginRight: "15px",
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
       }}
     >
-      <AppStoreBadges/>
-      <SocialBadges/>
+      <AppStoreBadges />
+      <SocialBadges />
     </div>
   );
 };
 
-export {
-  WebBarFooter,
-}
+export { WebBarFooter };
